baf4ab8fba6c4b5605da82c75fcafc667da20709
ntopulos <nt@uptn.net>
2022-06-07 17:31:09 +0200

    Add information on normal source processing errors

--- source_processing/normal.py
+++ source_processing/normal.py
@@ -12,28 +12,32 @@ def process_normal_source(data, source):
   print(f'Processing {source.name}...')
   projects = []
 
-  for ip in data:
-    project = Project(ip[0], source.id)
+  for idx, ip in enumerate(data):
+    try:
+      project = Project(ip[0], source.id)
 
-    project.development_stage = ip[1]
-    project.completion_year = ip[2]
-    project.dwellings_number = ip[3]
+      project.development_stage = ip[1]
+      project.completion_year = ip[2]
+      project.dwellings_number = ip[3]
 
-    # Address
-    project.address = ProjectAddress(source.country_code)
-    project.address.line1 = ip[4]
-    project.address.region = ip[5]
-    project.address.code = ip[6]
+      # Address
+      project.address = ProjectAddress(source.country_code)
+      project.address.line1 = ip[4]
+      project.address.region = ip[5]
+      project.address.code = ip[6]
 
-    # housing_tenure
-    for ht in literal_eval(ip[7]):
-      project.add_housing_tenure(ht)
+      # housing_tenure
+      for ht in literal_eval(ip[7]):
+        project.add_housing_tenure(ht)
 
-    # legal form
-    for lf in literal_eval(ip[8]):
-      project.add_legal_form(lf)
+      # legal form
+      for lf in literal_eval(ip[8]):
+        project.add_legal_form(lf)
 
-    projects.append(project)
+      projects.append(project)
+    except:
+      print(f'\nERROR: An error occured during the processing of the row {idx+2} of the source {source.name}.\n')
+      raise
 
   print(f'Processed: {len(projects)} projects\n')
   return projects
